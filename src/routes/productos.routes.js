17395ffd5657898f6063bafb11d7c3795be9c096
Augusto Brito <britoaugusto1@gmail.com>
2024-10-07 11:29:49 -0300

    agregamos las validaciones para el campo de categoria e imagen

--- src/routes/productos.routes.js
+++ src/routes/productos.routes.js
@@ -25,18 +25,34 @@ router
         .withMessage(
           "El nombre del producto debe contener como minimo 2 caracteres y como maximo 50 inclusive"
         ),
-        check("precio")
+      check("precio")
         .notEmpty()
-        .withMessage('El precio es un dato obligatorio')
+        .withMessage("El precio es un dato obligatorio")
         .isNumeric()
-        .withMessage('El precio debe ser un número')
-        .custom((valor)=>{
-         if(valor >= 50 && valor <= 20000){
-          return true;
-         }else{
-          throw new Error("El precio ingresado debe  estar entre $50 y $20000 inclusive");
-         }
-        })
+        .withMessage("El precio debe ser un número")
+        .custom((valor) => {
+          if (valor >= 50 && valor <= 20000) {
+            return true;
+          } else {
+            throw new Error(
+              "El precio ingresado debe  estar entre $50 y $20000 inclusive"
+            );
+          }
+        }),
+      check("imagen")
+        .notEmpty()
+        .withMessage("La imagen es un dato obligatorio")
+        .matches(/(http(s?):)([/|.|\w|\s|-])*\.(?:jpg|jpeg|gif|png)/)
+        .withMessage(
+          "La imagen debe ser una URL valida y terminar en alguna de las siguientes extensiones (jpg|jpeg|gif|png)"
+        ),
+      check("categoria")
+        .notEmpty()
+        .withMessage("La categoria es un dato obligatorio")
+        .isIn("infusiones", "salado", "dulce", "batidos")
+        .withMessage(
+          "La categoria debe contener una de las siguientes opciones : infusiones, salado, dulce, batidos"
+        ),
     ],
     crearProducto
   )
