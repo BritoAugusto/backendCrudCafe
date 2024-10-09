5347d33e53f3d5a4d7e6e2786ede44a5e67630a9
Augusto Brito <britoaugusto1@gmail.com>
2024-10-09 10:13:47 -0300

    agregamos las validaciones para password y para email en el modelo User

--- src/database/model/users.js
+++ src/database/model/users.js
@@ -1,4 +1,4 @@
-import mongoose, {Schema} from "mongoose";
+import mongoose, { Schema } from "mongoose";
 
 const userSchema = new Schema({
   nombreUser: {
@@ -12,23 +12,30 @@ const userSchema = new Schema({
     type: String,
     required: true,
     unique: true,
+    trim: true,
     minLength: 3,
     maxLength: 320,
-    validate:{
-        validator: (valor)=>{
-            return /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i;
-        }
-    }
+    validate: {
+      validator: (value) => {
+        return /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/.test(
+          value
+        );
+      },
+    },
   },
-  password:{
-type: String,
-required: true,
-minLength: 4,
-maxLength: 50
-  }
+  password: {
+    type: String,
+    required: true,
+    minLength: 4,
+    maxLength: 100,
+    trim: true,
+    validate: (value) => {
+      return /^(?=.*\d)(?=.*[\u0021-\u002b\u003c-\u0040])(?=.*[A-Z])(?=.*[a-z])\S{8,16}$/.test(
+        value
+      );
+    },
+  },
 });
 
-const  User = mongoose.model('User', userSchema);
-export default  User;
-
-
+const User = mongoose.model("User", userSchema);
+export default User;
