6b062cda4172ed0cfb95cc3fc193b190cfa9f2dd
Augusto Brito <britoaugusto1@gmail.com>
2024-10-14 10:24:20 -0300

    agregamos una funcion para verificar la veracidad del token

---
+++ src/helpers/verificarJWT.js
@@ -0,0 +1,38 @@
+import { jwt } from "jsonwebtoken";
+
+const verificarJWT = (req, res, next) => {
+  try {
+    console.log(req.header);
+    const token = req.header("x-token");
+    //no me enviaron el token?
+    if (!token) {
+      return res
+      .status(401)
+      .json({ mensaje: "No hay token de autenticacion" });
+    }
+    //el token si viene en el req
+    const payload = jwt.verify(token, process.env.SECRETO);
+    console.log(payload)
+    //continuo con la siguiente ejecucion por crearProd, borrarProd, editarProd
+    next();
+  } catch (error) {
+console.error('error al verificar el token')
+switch (error.name) {
+  case "JsonWebTokenError":
+    return res.status(401).json({ mensaje: "Token invalido" });
+  case "TokenExpiredError":
+    return res.status(401).json({ mensaje: "El token expiró" });
+  case "UnauthorizedError":
+    return res.status(403).json({ mensaje: "No tiene los permisos suficientes para acceder al recurso" });
+  default:
+    return res
+    .status(500)
+    .json({
+      mensaje: "Error al verificar el token",
+    });
+    break;
+}
+  }
+};
+
+export default  verificarJWT;
