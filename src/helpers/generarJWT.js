8f941051b204384456ccaaeb7832067471619f3b
Augusto Brito <britoaugusto1@gmail.com>
2024-10-14 09:59:36 -0300

    agregamos la funcion para generar un token

---
+++ src/helpers/generarJWT.js
@@ -0,0 +1,16 @@
+import { jwt } from "jsonwebtoken";
+
+const generarJWT = async (uid, email) => {
+  try {
+    const payload = { uid, email };
+    const token = await jwt.sign(payload, process.env.SECRET_JWT, {
+      expiresIn: "2h",
+    });
+    return token;
+  } catch (error) {
+    console.error("error al generar el token " + error.message);
+    throw new Error("No se pudo generar el token");
+  }
+};
+
+export default generarJWT;
